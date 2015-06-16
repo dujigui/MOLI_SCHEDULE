8740e787a677be63b30b7ad607eb3c334d2b0d85
pheynix <pheynix@163.com>
2015-06-16 10:49:11 +0800

    增加viewpager＋fragment前备份

--- app/src/main/java/com/pheynix/moli_schedule/MainActivity.java
+++ app/src/main/java/com/pheynix/moli_schedule/MainActivity.java
@@ -1,44 +1,127 @@
 package com.pheynix.moli_schedule;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private Toolbar mToolbar;
+    private NavigationView mNavigationView;
+    private ViewPager mViewPager;
+    private TabLayout tabs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.id_toolbar);
-        setSupportActionBar(toolbar);
+
+        initView();
+
+        setUpDrawer();
+
 
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void initView() {
+        //设置新的Toolbar
+        mToolbar = (Toolbar) findViewById(R.id.id_toolbar);
+        setSupportActionBar(mToolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.id_drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.id_navigationView);
+        //设置Viewpager和tabs
+        mViewPager = (ViewPager) findViewById(R.id.id_viewPager);
+        tabs = (TabLayout) findViewById(R.id.id_tabs);
+
+        tabs.addTab(tabs.newTab().setText(R.string.schedule));
+        tabs.addTab(tabs.newTab().setText(R.string.record));
+        tabs.addTab(tabs.newTab().setText(R.string.summary));
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    //设置汉堡包图标和菜单事件点击的响应逻辑
+    private void setUpDrawer() {
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
 
-        return super.onOptionsItemSelected(item);
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+//                不需要设置为check
+//                menuItem.setChecked(true);
+
+                switch (menuItem.getItemId()) {
+                    case R.id.id_drawer_share:
+                        Toast.makeText(getApplicationContext(), menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.id_drawer_settings:
+
+                        break;
+
+                    case R.id.id_drawer_about:
+
+                        break;
+
+                    case R.id.id_contact_us:
+
+                        break;
+                }
+
+                mDrawerLayout.closeDrawers();
+
+                return true;
+            }
+        });
+
+        mDrawerToggle.syncState();
+
     }
+
+//    删除PopUpMenu
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        super.onOptionsItemSelected(item);
+//
+//        switch (item.getItemId()) {
+//            case R.id.action_settings:
+//                break;
+//        }
+//
+//        return true;
+//    }
 }
