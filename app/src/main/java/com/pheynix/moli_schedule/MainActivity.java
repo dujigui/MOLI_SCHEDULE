29561c3fe92f4678fbf5c05815178d4c2cca223a
pheynix <pheynix@163.com>
2015-06-16 15:28:54 +0800

    增加viewpager，使用普通layout作为view； 改用fragment前备份；

--- app/src/main/java/com/pheynix/moli_schedule/MainActivity.java
+++ app/src/main/java/com/pheynix/moli_schedule/MainActivity.java
@@ -3,14 +3,20 @@ package com.pheynix.moli_schedule;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private NavigationView mNavigationView;
     private ViewPager mViewPager;
     private TabLayout tabs;
+    private List<View> views;
 
 
     @Override
@@ -46,10 +53,52 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.id_viewPager);
         tabs = (TabLayout) findViewById(R.id.id_tabs);
 
-        tabs.addTab(tabs.newTab().setText(R.string.schedule));
-        tabs.addTab(tabs.newTab().setText(R.string.record));
-        tabs.addTab(tabs.newTab().setText(R.string.summary));
+        String[] titles = getResources().getStringArray(R.array.tabs_titles);
+        tabs.addTab(tabs.newTab().setText(titles[0]));
+        tabs.addTab(tabs.newTab().setText(titles[1]));
+        tabs.addTab(tabs.newTab().setText(titles[2]));
 
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        views = new ArrayList<View>();
+        views.add(inflater.inflate(R.layout.layout_schedule,null));
+        views.add(inflater.inflate(R.layout.layout_record,null));
+        views.add(inflater.inflate(R.layout.layout_summary,null));
+
+        PagerAdapter mPagerAdapter = new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return views.size();
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView(views.get(position));
+            }
+
+            @Override
+            public int getItemPosition(Object object) {
+                return super.getItemPosition(object);
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return getResources().getStringArray(R.array.tabs_titles)[position];
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                container.addView(views.get(position));
+                return views.get(position);
+            }
+        };
+        mViewPager.setAdapter(mPagerAdapter);
+        tabs.setupWithViewPager(mViewPager);
     }
 
 
@@ -83,15 +132,15 @@ public class MainActivity extends AppCompatActivity {
                         break;
 
                     case R.id.id_drawer_settings:
-
+                        Toast.makeText(getApplicationContext(), menuItem.getTitle(), Toast.LENGTH_SHORT).show();
                         break;
 
                     case R.id.id_drawer_about:
-
+                        Toast.makeText(getApplicationContext(), menuItem.getTitle(), Toast.LENGTH_SHORT).show();
                         break;
 
                     case R.id.id_contact_us:
-
+                        Toast.makeText(getApplicationContext(), menuItem.getTitle(), Toast.LENGTH_SHORT).show();
                         break;
                 }
 
