8ee534d112b8211b16a7e1ee25818adc8d67503b
pheynix <pheynix@163.com>
2015-06-24 16:25:05 +0800

    change menu icon

--- app/src/main/java/com/pheynix/moli_schedule/SummarySchedule.java
+++ app/src/main/java/com/pheynix/moli_schedule/SummarySchedule.java
@@ -12,7 +12,7 @@ public class SummarySchedule extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_summary_schedule, container, false);
+        return inflater.inflate(R.layout.fragment_summary, container, false);
     }
 
 
