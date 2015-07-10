26c2df3716350dbf70bc7c3349dcc05ab913611a
pheynix <pheynix@163.com>
2015-07-10 15:49:34 +0800

    初步设计summary_daily的页面，准备修改数据库，把存储为TEXT的时间改变为INTEGER

--- app/src/main/java/com/pheynix/moli_schedule/PercentageView/PercentageView.java
+++ app/src/main/java/com/pheynix/moli_schedule/PercentageView/PercentageView.java
@@ -12,29 +12,67 @@ import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.pheynix.moli_schedule.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 
 public class PercentageView extends Fragment {
     private ImageView bg;
     private ImageView front;
+    private TextView tv_left;
+    private TextView tv_right;
     private GradientDrawable drawable_bg;
     private GradientDrawable drawable_front;
     private int height = 100;
     private int width = 10;
     private int temp_percentage;
 
+    private int color_pink = 0xffE91E63;
+    private int color_purple = 0xffE040FB;
+    private int color_purple_deep = 0xff7C4DFF;
+    private int color_indigo = 0xff536DFE;
+    private int color_blue = 0xff448AFF;
+    private int color_green = 0xff4CAF50;
+    private int color_yellow = 0xffFFEB3B;
+    private int color_orange = 0xffFF9800;
+    private int color_brown = 0xff795548;
+    private int color_blue_grey = 0xff607D8B;
+
+    public static final List<Integer> colors = new ArrayList<>();
+
 
     public PercentageView() {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_percentage_view, container, false);
         bg = (ImageView) view.findViewById(R.id.percentage_view_bg);
         front = (ImageView) view.findViewById(R.id.percentage_view_front);
+        tv_left = (TextView) view.findViewById(R.id.percentage_view_text_view_left);
+        tv_right = (TextView) view.findViewById(R.id.percentage_view_text_view_right);
+
+        for (int i = 0 ; i < 5 ; i++){
+
+            colors.add(color_yellow);
+            colors.add(color_green);
+            colors.add(color_brown);
+            colors.add(color_pink);
+            colors.add(color_blue);
+            colors.add(color_purple);
+            colors.add(color_orange);
+            colors.add(color_indigo);
+            colors.add(color_purple_deep);
+            colors.add(color_blue_grey);
+        }
+
+
 
         getWidthAndHeight();
 
@@ -48,9 +86,20 @@ public class PercentageView extends Fragment {
         @Override
         public void handleMessage(Message msg) {
 
-            switch (msg.what){
+            switch (msg.what) {
                 case 1:
                     front.setLayoutParams(new RelativeLayout.LayoutParams(width * temp_percentage / 100, height));
+
+                    if (temp_percentage < 91) {
+                        tv_right.setVisibility(View.VISIBLE);
+                        tv_right.setText(temp_percentage + "%");
+                        tv_left.setVisibility(View.GONE);
+                    } else {
+                        tv_left.setVisibility(View.VISIBLE);
+                        tv_left.setText(temp_percentage + "%");
+                        tv_right.setVisibility(View.GONE);
+                    }
+
                     break;
             }
 
@@ -58,12 +107,12 @@ public class PercentageView extends Fragment {
         }
     };
 
-    public void setPercentage(final int percentage){
+    public void setPercentage(final int percentage) {
 
 
         //非100%时，左边有5dp的倒角，右边无
         //非100%时，左边有5dp的倒角，右边有
-        if (percentage != 100){
+        if (percentage != 100) {
             float[] floats = {
                     (float) DimensionUtil.dpToPx(5), (float) DimensionUtil.dpToPx(5),
                     (float) DimensionUtil.dpToPx(0), (float) DimensionUtil.dpToPx(0),
@@ -71,19 +120,18 @@ public class PercentageView extends Fragment {
                     (float) DimensionUtil.dpToPx(5), (float) DimensionUtil.dpToPx(5)};
 
             drawable_front.setCornerRadii(floats);
-        }else {
+        } else {
             drawable_front.setCornerRadius(DimensionUtil.dpToPx(5));
         }
-
 
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0 ; i < percentage+1 ; i++){
+                for (int i = 0; i < percentage + 1; i++) {
                     try {
                         temp_percentage = i;
-                        Thread.sleep(10);
+                        Thread.sleep(20);
                         Message message = new Message();
                         message.what = 1;
                         handler.sendMessage(message);
@@ -97,8 +145,8 @@ public class PercentageView extends Fragment {
 //        front.setLayoutParams(new RelativeLayout.LayoutParams(width * percentage / 100, height));
     }
 
-//    http://stackoverflow.com/questions/17823451/set-android-shape-color-programmatically
-    public void setColor(int color){
+    //    http://stackoverflow.com/questions/17823451/set-android-shape-color-programmatically
+    public void setColor(int color) {
 
         drawable_bg = (GradientDrawable) bg.getBackground();
         drawable_bg.setColor(color);
@@ -111,17 +159,17 @@ public class PercentageView extends Fragment {
     }
 
     //应该添加public static final int MATCH_PARENT;
-    public void setWidth(int dp){
-        bg.setLayoutParams(new RelativeLayout.LayoutParams(DimensionUtil.dpToPx(dp),height));
+    public void setWidth(int dp) {
+        bg.setLayoutParams(new RelativeLayout.LayoutParams(DimensionUtil.dpToPx(dp), bg.getLayoutParams().height));
     }
 
-    public void setHeight(int dp){
-        bg.setLayoutParams(new RelativeLayout.LayoutParams(width,DimensionUtil.dpToPx(dp)));
-        front.setLayoutParams(new RelativeLayout.LayoutParams(width,DimensionUtil.dpToPx(dp)));
+    public void setHeight(int dp) {
+        bg.setLayoutParams(new RelativeLayout.LayoutParams(bg.getLayoutParams().width, DimensionUtil.dpToPx(dp)));
+        front.setLayoutParams(new RelativeLayout.LayoutParams(bg.getLayoutParams().width, DimensionUtil.dpToPx(dp)));
     }
 
 
-    private void getWidthAndHeight(){
+    private void getWidthAndHeight() {
         ViewTreeObserver viewTreeObserver = bg.getViewTreeObserver();
         viewTreeObserver.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
             @Override
@@ -133,5 +181,30 @@ public class PercentageView extends Fragment {
                 return true;
             }
         });
+    }
+
+
+    //产生随机的颜色序列，不是很需要
+    private void getColors() {
+        Random random = new Random();
+        List<Integer> numbers = new ArrayList<>();
+        List<Integer> colors = new ArrayList<>();
+
+        while (numbers.size() < 11) {
+            int number = random.nextInt(10);
+            if (!numbers.contains(number)) {
+                numbers.add(number);
+            }
+        }
+
+        for (int i = 0 ; i < numbers.size(); i++) {
+            colors.add(this.colors.get(numbers.get(i)));
+        }
+
+        this.colors.clear();
+
+        for (int i = 0 ; i < 5 ; i++){
+            this.colors.addAll(colors);
+        }
     }
 }
--- app/src/main/java/com/pheynix/moli_schedule/SummaryDailyFragment.java
+++ app/src/main/java/com/pheynix/moli_schedule/SummaryDailyFragment.java
@@ -3,12 +3,9 @@ package com.pheynix.moli_schedule;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.pheynix.moli_schedule.PercentageView.PercentageView;
 
@@ -18,76 +15,25 @@ import com.pheynix.moli_schedule.PercentageView.PercentageView;
  */
 public class SummaryDailyFragment extends Fragment {
     private View view;
-
+    private PercentageView pv_complete_ratio_daily,pv_time_summary_daily;
 
     public SummaryDailyFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_summary_daily, container, false);
+        pv_complete_ratio_daily = (PercentageView) getChildFragmentManager().findFragmentById(R.id.pv_complete_ratio_daily);
+        pv_time_summary_daily = (PercentageView) getChildFragmentManager().findFragmentById(R.id.pv_time_summary_daily);
 
-        final PercentageView percentageView = (PercentageView) getChildFragmentManager().findFragmentById(R.id.daily_percentage_view);
+        pv_complete_ratio_daily.setPercentage(47);
+        pv_time_summary_daily.setPercentage(78);
 
-        if (((PercentageView) getChildFragmentManager().findFragmentById(R.id.daily_percentage_view)) == null){
-            Log.e("pheynix","为毛");
-        }
+        pv_complete_ratio_daily.setColor(PercentageView.colors.get(0));
+        pv_time_summary_daily.setColor(PercentageView.colors.get(1));
 
-//        SeekBar seekBar = (SeekBar) view.findViewById(R.id.seekBar);
-//        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-//            @Override
-//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-//                percentageView.setPercentage(progress);
-//            }
-//
-//            @Override
-//            public void onStartTrackingTouch(SeekBar seekBar) {
-//
-//            }
-//
-//            @Override
-//            public void onStopTrackingTouch(SeekBar seekBar) {
-//
-//            }
-//        });
-
-        final EditText editText = (EditText) view.findViewById(R.id.et_percentage);
-        Button button = (Button) view.findViewById(R.id.btn_change_percentage);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                percentageView.setPercentage(Integer.parseInt(editText.getText().toString())
-                percentageView.setPercentage(Integer.parseInt(editText.getText().toString()));
-            }
-        });
 
         return view;
-    }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-//        final PercentageView percentageView = (PercentageView) getFragmentManager().findFragmentById(R.id.daily_percentage_view);
-//        SeekBar seekBar = (SeekBar) view.findViewById(R.id.seekBar);
-//        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-//            @Override
-//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-//                percentageView.setPercentage(progress);
-//            }
-//
-//            @Override
-//            public void onStartTrackingTouch(SeekBar seekBar) {
-//
-//            }
-//
-//            @Override
-//            public void onStopTrackingTouch(SeekBar seekBar) {
-//
-//            }
-//        });
-
     }
 }
